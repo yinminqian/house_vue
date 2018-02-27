75cb83fe523facf794be23a2044403fa050903e1
尹敏乾 <yinminqian@yinminqiandeiMac.local>
2018-02-27 23:26:39 +0800

    sev is ok

--- .cfg.js
+++ .cfg.js
@@ -1,4 +1,4 @@
 export default {
-  api:'http://localhost:2333'
-  // api:'http://house-api.yinminqian.biaoyansu.com'
+  // api:'http://localhost:2333'
+  api:'http://house-api.yinminqian.biaoyansu.com'
 }
